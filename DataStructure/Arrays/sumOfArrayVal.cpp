9a4f8f6f0b4960dd38c62ed2ea2be81a56ec5b11
anujnaRao <anujna.prao5974@yahoo.com>
2020-08-23 23:25:50 +0530

    Array values sum

---
+++ DataStructure/Arrays/sumOfArrayVal.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// Using the basic array addition with for loop
+
+// int main(){
+
+//     int n, ar[n], sum = 0;
+//     cout << "Enter the value of n \n";
+//     cin >> n;
+//     cout << "Enter the array elements: \n";
+//     for(int i=0;i<n;i++)
+//         cin >> ar[i]; 
+    
+//     cout << "Sum is: \n";
+//     for(int i=0;i<n;i++){
+//         sum+= ar[i];
+//     }
+//      cout << sum;
+// }
+
+int main(){
+    
+}
