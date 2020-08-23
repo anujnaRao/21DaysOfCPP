c029ce85560ab974e4bdcd27f8a968b32a5b4707
anujnaRao <anujna.prao5974@yahoo.com>
2020-08-23 23:50:23 +0530

    using numeric or vector from STL

--- DataStructure/Arrays/sumOfArrayVal.cpp
+++ DataStructure/Arrays/sumOfArrayVal.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
+#include <numeric>
+#include <vector>
 
 using namespace std;
 
-// Using the basic array addition with for loop
 
-// int main(){
+    int main(){
 
-//     int n, ar[n], sum = 0;
-//     cout << "Enter the value of n \n";
-//     cin >> n;
-//     cout << "Enter the array elements: \n";
-//     for(int i=0;i<n;i++)
-//         cin >> ar[i]; 
-    
+        int n, ar[n], sum = 0;
+        vector<int> v{10, 20, 30};
+
+        // cout << "Enter the value of n \n";
+        // cin >> n;
+        // cout << "Enter the array elements: \n";
+        // for(int i=0;i<n;i++)
+        //     cin >> ar[i]; 
+// Using the basic array addition with for loop  
 //     cout << "Sum is: \n";
 //     for(int i=0;i<n;i++){
 //         sum+= ar[i];
 //     }
 //      cout << sum;
-// }
 
-int main(){
-    
+// Using numeric header from STL
+    // cout << "Sum is: \n" << accumulate(ar, ar+n, sum);
+
+// Using vector header from STL
+     cout << "Sum is: \n" << accumulate(v.begin(), v.end(), sum);
+
 }
