cd6c4ff47654d1cd57416d3758e4b07e30203ff7
anujnaRao <anujna.prao5974@yahoo.com>
2020-09-03 17:00:21 +0530

    Linera Search

---
+++ DataStructure/Algorithms/Searching/LinearSearch.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, ar[n],flag = 0, ele;
+    
+    cin >> n; //Size of array
+
+    for(int i = 0; i < n; i++)
+        cin >> ar[i]; //array elements
+    
+    cin >> ele; // Search element
+    
+    for(int i = 0; i < n; i++){
+        if( ar[i] == ele){
+            flag = i+1;
+            break;
+        }
+        else
+        {
+            flag =0;
+        }
+    }
+    if( flag != 0)
+        cout << "Element is found in position: " << flag << endl;
+    else
+        cout << "The element is not found in the list \n";
+    return 0;
+}
