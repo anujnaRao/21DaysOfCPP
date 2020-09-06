e5affc8a0b122ad3812c7bcec761e3f4e9d701e1
anujnaRao <anujna.prao5974@yahoo.com>
2020-09-06 20:26:55 +0530

    Shell Sort

---
+++ Algorithms/Sorting/ShellSort.cpp
@@ -0,0 +1,52 @@
+/*
+Improvisation os insertion sort
+Comparision is done of elements within 
+gap of several elements
+Time Complexity: Omega(n log n) B.C
+Theta(n log n^2)A.C
+O(n log(n)^2) W.C
+Space: O(1)
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void shellsort(int arr[], int num)    
+{    
+    int i, j, k, tmp;    
+    for (i = num / 2; i > 0; i = i / 2)    
+    {    
+        for (j = i; j < num; j++)    
+        {    
+            for(k = j - i; k >= 0; k = k - i)    
+            {    
+                if (arr[k+i] >= arr[k])    
+                    break;    
+                else    
+                {    
+                    tmp = arr[k];    
+                    arr[k] = arr[k+i];    
+                    arr[k+i] = tmp;    
+                }    
+            }    
+        }    
+    }    
+}    
+int main()    
+{    
+    int arr[30];    
+    int k,  num;    
+    printf("Enter total no. of elements : ");    
+    scanf("%d", &num);    
+    printf("\nEnter %d numbers: ", num);    
+    
+    for (k =  0 ; k < num; k++)    
+    {    
+        scanf("%d", &arr[k]);    
+    }    
+    shellsort(arr, num);    
+    printf("\n Sorted array is: ");    
+    for (k = 0; k < num; k++)    
+        printf("%d ", arr[k]);    
+    return 0;    
+}    
